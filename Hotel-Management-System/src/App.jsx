abb7af40aaf2dce1da35dda12488e4363c62e100
Sanduni <sanduniwathsala0123@gmail.com>
2024-06-03 10:48:42 +0530

    First commit

---
+++ Hotel-Management-System/src/App.jsx
@@ -0,0 +1,51 @@
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import BaseLayout from "./layouts/BaseLayout"
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BaseLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <BaseLayout />,
+    children: [
+      {
+        index: true,
+        element: <LoginPage />,
+      },
+    ],
+  },
+  {
+    path: "/sign-up",
+    element: <BaseLayout />,
+    children: [
+      {
+        index: true,
+        element: <SignUpPage />,
+      },
+    ],
+  },
+ 
+]);
+
+function App() {
+  return (
+    <div>
+        <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
