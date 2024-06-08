6ec13a67df2585b0ffe552ac3646e674a107d908
S19387 <dasuni2018@gmail.com>
2024-06-08 18:52:15 +0530

    edited again

--- Hotel-Management-System/src/layouts/BaseLayout.jsx
+++ Hotel-Management-System/src/layouts/BaseLayout.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import { Outlet } from "react-router-dom";
+import { Outlet,Link } from "react-router-dom";
 
 const BaseLayout = () => {
   return (
-    <div> <Outlet /></div>
-  )
-}
-
-export default BaseLayout
+    <div className="flex flex-col min-h-screen">
+    <header className="bg-blue-500 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <nav>
+          <ul className="flex space-x-4">
+            <li><Link to="/" className="hover:underline">Home</Link></li>
+            <li><Link to="/login" className="hover:underline">Login</Link></li>
+            <li><Link to="/sign-up" className="hover:underline">SignUp</Link></li>
+            <li><Link to="/servies" className="hover:underline">Services</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <main className="flex-grow p-4">
+      <Outlet />
+    </main>
+  </div>
+);
+}; 
+  
+export default BaseLayout;
