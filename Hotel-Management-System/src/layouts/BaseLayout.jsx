1a803055ced984e60a4200e28a3eeb2d26f395b0
sanduni4822 <sanduniwathsala0123@gmail.com>
2024-06-08 19:44:16 -0700

    Styling nav bar

--- Hotel-Management-System/src/layouts/BaseLayout.jsx
+++ Hotel-Management-System/src/layouts/BaseLayout.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import { Outlet,Link } from "react-router-dom";
+import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 const BaseLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
-    <header className="bg-blue-500 text-white p-4">
-      <div className="container mx-auto flex justify-end">
-        <nav>
-          <ul className="flex space-x-4">
-            <li><Link to="/" className="hover:underline">Home</Link></li>
-            <li><Link to="/rooms" className="hover:underline">Rooms</Link></li>
-            <li><Link to="/activities" className="hover:underline">Activities</Link></li>
-            <li><Link to="/services" className="hover:underline">Services</Link></li>
-            <li><Link to="/facilities" className="hover:underline">Facilities</Link></li>
-            <li><Link to="/contact" className="hover:underline">Contact</Link></li>
-            <li><Link to="/login or join" className="hover:underline">Login/Join</Link></li>
-            <li><Link to="/sign-up" className="hover:underline">SignUp</Link></li>
-            <li><Link to="/book now" className="hover:underline">Book Now!</Link></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-    <main className="flex-grow p-4">
-      <Outlet />
-    </main>
-  </div>
-);
+      <header className="bg-black text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <nav className="flex-1">
+            <ul className="flex space-x-4">
+              <li><Link to="/" className="hover:underline">Home</Link></li>
+              <li><Link to="/rooms" className="hover:underline">Rooms</Link></li>
+              <li><Link to="/activities" className="hover:underline">Activities</Link></li>
+              <li><Link to="/services" className="hover:underline">Services</Link></li>
+              <li><Link to="/facilities" className="hover:underline">Facilities</Link></li>
+              <li><Link to="/contact" className="hover:underline">Contact</Link></li>
+            </ul>
+          </nav>
+          <nav>
+            <ul className="flex space-x-4">
+              <li><Link to="/login" className="hover:underline">Login</Link></li>
+              <li><Link to="/sign-up" className="hover:underline">SignUp</Link></li>
+              <li><Link to="/book-now" className="hover:underline">Book Now!</Link></li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <main className="flex-grow p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
 }; 
   
 export default BaseLayout;
